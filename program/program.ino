2384fef64b1b22985c006f7f60c4d77e6d5dd986
Sam Daitzman <samuel@daitzman.com>
2015-02-22 14:45:26 -0500

    adds loopDelay and better comments

--- program/program.ino
+++ program/program.ino
@@ -6,9 +6,18 @@
 #import "variables.ino" // go to this file to set variables
 
 void setup() {
-  Serial.begin(57600);
+  delay(500); // reduces Serial issues
+  Serial.begin(57600); // not 9600 - so it's fast
+  Serial.print("Program started. Using Arduino-Boilerplate v");
+  Serial.println(arduinoBPVersion);
+  // put your setup code below
+
+
+
+  Serial.println("End setup.");
+
 }
 
 void loop() {
-  delay(100);
+  delay(loopDelay); // set in variables.ino and defaults to 100
 }
--- program/variables.ino
+++ program/variables.ino
@@ -1,3 +1,4 @@
 // variables and constants go in here
 
 #define arduinoBPVersion "0.0.1"
+#define loopDelay 100
